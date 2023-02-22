dd6ec29660947fbece2c9a9ab8a088f5696e3384
irenetjrs <irkapidgorodetska@gmail.com>
2023-02-23 00:53:04 +0200

    task 2

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ Object.defineProperty(user, 'id',{
 // Object.isExtensible(user.name);
 // Object.isExtensible(user.age);
 // Object.isSealed(user.id);
+
+// task 2
+
+let dataBase = {
+   dbName: 'user',
+   dbType: 'MySQL'
+}
+let configurateDB = {
+   token: '123',
+   password: '567',
+   user: 'admin'
+}
+Object.freeze(dataBase);
+Object.seal(configurateDB);
+console.log(Object.isFrozen(dataBase));
+console.log(Object.isSealed(configurateDB));
+
+
